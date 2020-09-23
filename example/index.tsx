65bae8f1840261475ff10b30efe57c482134eaca
Mikel Arnaiz <mikel.arnaiz@philips.com>
2020-09-23 19:43:11 +0200

    Adds info to Readme

--- example/index.tsx
+++ example/index.tsx
@@ -3,15 +3,19 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { useColorScheme } from '../src/useColorScheme';
 import { useIsDarkMode } from '../src/useIsDarkMode';
+import { ColorScheme } from '../src/ColorScheme';
+import { useIsColorScheme } from '../src/useIsColorScheme';
 
 const App = () => {
-  const colorScheme = useColorScheme();
   const isDarkMode = useIsDarkMode();
+  const colorScheme = useColorScheme();
+  const isLightMode = useIsColorScheme(ColorScheme.Light);
 
   return (
     <div>
-      <p>Color scheme: {colorScheme}</p>
       <p>Is dark mode? {isDarkMode ? 'Yeah' : 'Noop'}</p>
+      <p>Color scheme: {colorScheme}</p>
+      <p>Is light mode? {isLightMode ? 'Yeah' : 'Noop'}</p>
     </div>
   );
 };
